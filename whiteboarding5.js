0ca18088330b8983542cfa1a68e9a42930a00b31
Brandon Turner <brandontheturner@gmail.com>
2019-03-28 19:29:58 -0500

    whiteboarding and updates

---
+++ whiteboarding5.js
@@ -0,0 +1,9 @@
+// take an array of prices where each index represents a time, ie every minute or every hour
+// build an algorithm to find out what your best profit could have been if you bought at the lowest price and sold at the highest price, after you bought. You must buy before you sell. 
+
+const myArr = [75, 39, 44, 1, 8, 10, 1, 11, 2, 3,4,5]
+findBestSellAndBuy{myArr} => {
+  maxProfit: 10 buy @myArr[3] for 1 and sell myArr[7] for 11
+}
+
+//use bubble sort
